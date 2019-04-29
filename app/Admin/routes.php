95ad5742abdd8725fe1ef50e88930af940c886bb
yuanshuai <yuanshuai@localhost.localdomain>
2019-04-29 01:21:10 +0000

    code.js

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,4 +13,5 @@ Route::group([
     $router->get('/', 'HomeController@index')->name('admin.home');
     $router->resource("/goods",GoodsController::class);//商品管理
     $router->resource("/users",UserController::class);//用户管理
+    $router->resource("/sucai/add",SucaiController::class);//添加素材
 });
